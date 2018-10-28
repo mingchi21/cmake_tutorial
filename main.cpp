df3c5c74bdb9adfb077adb6300f1a347c3421f1c
mingchi21 <mingchi21@gmail.com>
2018-10-28 22:38:36 +0800

    Init version

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bubbleSort.h"
+
+int number[] = {10, 9, 1, 2, 5, 3, 8, 7}; 
+static void _print(int* arr, int len) {
+    int i;
+    for(i = 0; i < len; i++) { 
+        printf("%d ", arr[i]); 
+    } 
+    printf("\n");
+}
+
+
+int main(int, char**) {    
+    int len = sizeof(number)/sizeof(int);
+
+    bubbleSort(number, len, descending);
+    _print(number, len);
+}
